2357daf4b702358ff17dc29c25b6a9c7889ebc1d
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2015-03-19 23:28:58 -0400

    Fix bug + implement prettyPrint

--- list.c
+++ list.c
@@ -3,16 +3,47 @@
 Node* head;
 Node* curr;
 
-void add(int number){
+void add(int number)
+{
+	if(curr == NULL)
+	{
+		curr = malloc(sizeof(int) + sizeof(Node*));
+		curr->value = number;
+		head->next = curr;
+		return;
+	}
+
 	Node* n = malloc(sizeof(int) + sizeof(Node*));
+	n->value = number;
 	curr->next = n;
 	curr = n;
 }
 
+
+void prettyPrint()
+{
+	Node* loop = head;
+	while(loop != NULL)
+	{
+		printf("%d ", loop->value);
+		loop = loop->next;
+	}
+
+	printf("\n");
+}
+
+
 int main(){
 
-	curr = malloc(sizeof(int) + sizeof(Node*));	
-	curr->value = 5;
-	printf("%d", curr->value);
+	head = malloc(sizeof(int) + sizeof(Node*));	
+	head->value = 5;
+
+	add(4);
+	add(3);
+	add(2);
+	add(1);
+
+	prettyPrint();
+	//printf("%d", curr->value);
 	return 0;
 }
